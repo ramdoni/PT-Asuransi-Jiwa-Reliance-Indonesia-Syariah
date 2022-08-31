fb5232daa873106005aeb798c5af3bead2a304b6
Ramdoni <doni.enginer@gmail.com>
2022-08-31 21:43:48 +0700

    update: pengajuan

--- app/Helpers/GeneralHelper.php
+++ app/Helpers/GeneralHelper.php
@@ -80,19 +80,24 @@ function hitung_masa($start,$end){
 }
 
 function hitung_masa_bulan($start,$end,$masa_asuransi=1){
+
     $birthDate = new \DateTime($start);
 	$today = new \DateTime($end);
 	if ($birthDate > $today) { 
 	    return 0;
     }
 
+    $today->modify('-1 day');
+
     if($masa_asuransi==2) $today->modify('+1 day');
 
     $tahun = $today->diff($birthDate)->y;
 
-    $bulan =  ($tahun*12) + $today->diff($birthDate)->m;
+    $bulan = $today->diff($birthDate)->m;
+    if($tahun>0) $bulan += $tahun*12;
+    
     $hari = $today->diff($birthDate)->d;
-
+    return $bulan;
     if($hari >0)
         return $bulan + 1;
     else
--- app/Http/Controllers/PengajuanController.php
+++ app/Http/Controllers/PengajuanController.php
@@ -17,10 +17,10 @@ class PengajuanController extends Controller
         $extra_kontribusi = $id->kepesertaan->where('status_akseptasi',1)->sum('extra_kontribusi');
         $extra_mortalita = $id->kepesertaan->where('status_akseptasi',1)->sum('extra_mortalita');
 
-        $total = $kontribusi+$extra_kontribusi+$extra_mortalita+$id->biaya_sertifikat+$id->pph+$id->ppn-$id->potong_langsung;
-
+        $total = $kontribusi+$extra_kontribusi+$extra_mortalita+$id->biaya_sertifikat+$id->biaya_polis_materai+$id->pph - ($id->ppn+$id->potong_langsung);
+        $total_gross = $kontribusi+$extra_kontribusi+$extra_mortalita;
         $pdf = \App::make('dompdf.wrapper');
-        $pdf->loadView('livewire.pengajuan.print-dn',['extra_mortalita'=>$extra_mortalita,'total'=>$total,'data'=>$id,'head_teknik'=>$head_teknik,'kontribusi'=>$kontribusi,'extra_kontribusi'=>$extra_kontribusi,'potongan_langsung'=>$id->potongan_langsung]);
+        $pdf->loadView('livewire.pengajuan.print-dn',['total_gross'=>$total_gross,'extra_mortalita'=>$extra_mortalita,'total'=>$total,'data'=>$id,'head_teknik'=>$head_teknik,'kontribusi'=>$kontribusi,'extra_kontribusi'=>$extra_kontribusi,'potongan_langsung'=>$id->potongan_langsung]);
 
         return $pdf->stream();
     }
