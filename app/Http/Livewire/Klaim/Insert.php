90f4391acf1d06cf56b6fc83ec4005569c0759d0
Ramdoni <doni.engine@gmail.com>
2022-09-09 11:41:04 +0700

    init klaim

---
+++ app/Http/Livewire/Klaim/Insert.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Klaim;
+
+use Livewire\Component;
+
+class Insert extends Component
+{
+    public function render()
+    {
+        return view('livewire.klaim.insert');
+    }
+}
